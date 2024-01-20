ad8f188f1a3d1ed4608626d553e626fff296b179
Valentina Sant <75807211+valentinasant99@users.noreply.github.com>
2024-01-20 11:57:59 +0100

    Delete Storia2024 directory

--- Storia2024/src/main/java/com/example/sweng/client/services/register/RegisterServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.example.sweng.client.services.register;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface RegisterServiceAsync {
-
-    void register(String email, String password, AsyncCallback<Boolean> callback);
-
-}
--- Storia2024/src/main/java/com/example/sweng/shared/storia/scenario/ScenarioGenerale.java
+++
@@ -1,6 +0,0 @@
-package com.example.sweng.shared.storia.scenario;
-
-public interface ScenarioGenerale {
-    String getDescrizione();
-    void setDescrizione(final String descrizione);
-}
